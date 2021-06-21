fdf9f1a3db994b1790a9fd15af6b3e978a4ad9d7
AnbuStyle <khumaun99@gmail.com>
2021-06-21 03:04:33 +0300

    final

--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -2,4 +2,36 @@ package ru.geekbrains;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        String[][] arr = {{"1", "2", "3", "4"},
+                          {"1", "2", "3", "4"},
+                          {"1", "2", "3", "4"},
+                          {"1", "2", "y", "4"},
+
+        };
+        System.out.println(mass(arr));
+    }
+
+    public static int mass(String[][] data) {
+        if (data.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < 4; i++) {
+            if (data[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(data[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i+1,j+1);
+                }
+            }
+        }
+        return sum;
+    }
 }
